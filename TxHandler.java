dde314ec4da9dd34500eb0bcf2972767ec89563c
Stanislav Khurtesov <s.khurtesov@gmail.com>
2022-05-29 14:49:59 +0400

    Implemet isValidTx()

--- TxHandler.java
+++ TxHandler.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+
 public class TxHandler {
+    private UTXOPool utxoPool;
 
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
@@ -6,7 +9,7 @@ public class TxHandler {
      * constructor.
      */
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        this.utxoPool = new UTXOPool(utxoPool);
     }
 
     /**
@@ -19,7 +22,58 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
+        ArrayList<Transaction.Input> inputs = tx.getInputs();
+        double inputSum = 0;
+        for (int i = 0; i < inputs.size(); i++) {
+            Transaction.Input input = inputs.get(i);
+            UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
+            
+            // (1) all outputs claimed by {@code tx} are in the current UTXO pool
+            Transaction.Output prevOutput = utxoPool.getTxOutput(utxo);
+            if (prevOutput == null) {
+                return false;
+            }
+
+            inputSum += prevOutput.value;
+
+            // (2) the signatures on each input of {@code tx} are valid
+            byte[] message = tx.getRawDataToSign(i);
+            byte[] signature = input.signature;
+            if (! Crypto.verifySignature(prevOutput.address, message, signature)) {
+                return false;
+            }
+
+            // (3) no UTXO is claimed multiple times by {@code tx}
+            for (int j = i + 1; j < inputs.size(); j++) {
+                Transaction.Input anotherInput = inputs.get(j);
+                if (input.prevTxHash == anotherInput.prevTxHash && 
+                    input.outputIndex == anotherInput.outputIndex) {
+                    return false;
+                }
+            }
+        }
+
+        ArrayList<Transaction.Output> outputs = tx.getOutputs();
+        double outputSum = 0;
+        for (Transaction.Output output : outputs) {
+            double outputValue = output.value;
+
+            // (4) all of {@code tx}s output values are non-negative
+            if (outputValue < 0) {
+                return false;
+            }
+
+            outputSum += outputValue;
+        }
+
+        // (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+        //     values; and false otherwise.
+        if (inputSum >= outputSum) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /**
@@ -29,6 +83,7 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
+        return null;
     }
 
 }
